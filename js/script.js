f74ee8d0978435b8f87bd06fe996dbddb7abd965
paolotrimini <paolo.trimini@gmail.com>
2021-03-17 15:15:59 +0100

    numeri casuali

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+/*
+Un alert() espone 5 numeri generati casualmente. Da li parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
+tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali
+dei numeri da indovinare sono stati individuati.
+*/
+
+
+var myArr = [];
+
+
+// array di 5 numeri rnd
+while (myArr.length < 5) {
+
+    var rndNum = getRnd(1, 100);
+
+    if (myArr.includes(rndNum)) {
+        console.log('Numero già inserito');
+    } else {
+        myArr.push(rndNum);
+    }
+}
+//console.log('Numeri casuali: ' + myArr);
+alert('Numeri casuali: ' + myArr);
+
+
+
+
+    // timer di 30 sec
+   /* function insertNumbers() {
+        var myNumbers = parseInt(prompt('inserisci i numeri: '));
+    }
+
+    function init() {
+        setTimeout(insertNumbers, 3 * 1000); // 3 sec ( poi mettere 30 secondi)
+    }
+    init();*/
+
+
+
+
+
+// FUNZIONI
+
+function getRnd(min, max) {
+    var minRnd = min;
+    var maxRnd= max - minRnd + 1;
+    var rnd = Math.floor(Math.random() * maxRnd) +minRnd;
+    return rnd;
+}
+
+
